a668cdee821fbcfc3de03da92a53369f9c7cf130
oqoqoh <oqoq59@gmail.com>
2023-08-19 23:54:41 +0900

    📝 study js function code

---
+++ JS/study/function.js
@@ -0,0 +1,26 @@
+// Early return, early exit
+//bad
+function upgradeUser(user) {
+    if (user.point > 10) {
+        //long upgrader logic...
+    }
+}
+
+//good
+function upgradeuser2(user) {
+    if (user.point <= 10) {
+        return;
+    }
+    // long upgrade logic...
+}
+
+//Function expression
+// a function declaration can be called earlier than it is defiend. (hoisted)
+// a function expression is created when the execution reaches it.
+const print = function () {
+    //anonymous function
+    console.log('print;');
+};
+print();
+const printAgain = print;
+printAgain();
