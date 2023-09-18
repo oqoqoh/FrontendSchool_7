70898a72713371a81fdb2cf6c86d26331fced547
oqoqoh <oqoq59@gmail.com>
2023-09-19 01:38:26 +0900

    🚚  1만시간 페이지 컴포넌트

---
+++ React/hours-react/src/App.js
@@ -0,0 +1,16 @@
+import Footer from './components/footer/Footer';
+import Header from './components/header/Header';
+import Main from './components/main/Main';
+import Modal from './components/modal/Modal';
+
+function App() {
+    return (
+        <div id="app">
+            <Header />
+            <Main />
+            <Footer />
+            <Modal />
+        </div>
+    );
+}
+export default App;
